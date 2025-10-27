67275d1d5d1b8ccea597beee50e648be4aff56ed
renfei-Zhou <tobyzrf1997@gmail.com>
2025-10-28 00:11:05 +0100

    pub sub testing project stage-2

--- src/pkg_pub_sub/pkg_pub_sub/speed_controller.py
+++ src/pkg_pub_sub/pkg_pub_sub/speed_controller.py
@@ -13,10 +13,21 @@ class SpeedControllerNode(Node): # [node name] got
     def __init__(self):
         super().__init__("speed_controller") # node name to call
         self.subscriber_ = self.create_subscription(Float32, "speed_monitor", self.get_speed, 200)
-        self.get_logger().info("Speed Monitor is on.")
+        self.publisher_ = self.create_publisher(Float32, "speed_correction", 200) # topic tpye, topic name
+        self.get_logger().info("Speed Monitor is running.")
 
     def get_speed(self, value:Float32):
-        self.get_logger().info(f"{value.data:.2f}")
+        self.get_logger().info(f"received speed: {value.data:.2f}")
+        msg = Float32()
+
+        if value.data > 5.0:
+            msg.data = -0.12
+        elif value.data < 4.9:
+            msg.data = 0.05
+        else:
+            msg.data = 0.0
+        self.publisher_.publish(msg)
+
 
              
 
--- src/pkg_pub_sub/pkg_pub_sub/wheels.py
+++ src/pkg_pub_sub/pkg_pub_sub/wheels.py
@@ -13,17 +13,27 @@ class WheelsNode(Node): # [node name] got
     def __init__(self):
         super().__init__("wheels") # node name to call
         self.speed_generator_ = 0.0
+
         self.publisher_ = self.create_publisher(Float32, "speed_monitor", 200) # topic tpye, topic name
-        self.timer_ = self.create_timer(0.05, self.speed_value)
+        self.subscriber_ = self.create_subscription(Float32, "speed_correction", self.correction_callback, 200)
 
-        self.get_logger().info("Speed UPPPPP")
+        self.timer_ = self.create_timer(0.05, self.publish_speed)
+        self.speed_correction_ = 0.0
 
-    def speed_value(self):
-        speed_value = Float32()
-        speed_value.data = self.speed_generator_
-        self.publisher_.publish(speed_value)
-        self.speed_generator_ += 0.05
+        self.get_logger().info("Speed UPPPPPPPPPPP")
 
+
+    def correction_callback(self, corr:Float32):
+        self.speed_correction_ = corr.data
+
+
+    def publish_speed(self):
+        self.speed_generator_ += 0.05 + self.speed_correction_
+
+        msg = Float32()
+        msg.data = self.speed_generator_
+        self.publisher_.publish(msg)
+        
 
 def main(args=None):
     rclpy.init(args=args)
