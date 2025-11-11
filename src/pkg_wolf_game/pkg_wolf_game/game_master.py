55324f0bbc2f6a71a0322039f6368c0a32b9d90d
renfei-Zhou <tobyzrf1997@gmail.com>
2025-11-11 22:07:24 +0100

    change to 5 players, finish night phase

--- src/pkg_wolf_game/pkg_wolf_game/game_master.py
+++ src/pkg_wolf_game/pkg_wolf_game/game_master.py
@@ -2,7 +2,7 @@ import rclpy
 from rclpy.node import Node
 from example_interfaces.msg import String
 import random
-
+import time
 
 
 
@@ -11,22 +11,36 @@ class GameMasterNode(Node): # MODIFY NAME
     def __init__(self):
         super().__init__("game_master") # MODIFY NAME
         self.players_ready_ = []
+        self.player_states_ = {} 
+        self.player_roles_ = {}    
+        self.player_action_ = {} 
+        self.wolf_action_ = {} 
+
         self.roles_assigned_ = False
-        self.roles_ = ['villager','villager','wolf','wolf','witch','perdictor']
+        self.game_phase_ = "waiting"  # can be 'waiting', 'night', 'day', 'end'
+        self.roles_ = ['villager','villager','villager','villager','wolf'] # ['villager','villager','wolf','wolf','witch','perdictor']
         self.ready_sub_ = self.create_subscription(String, 'player_ready', self.player_ready_callback, 10) # topic name
         self.roles_pub_ = self.create_publisher(String, 'assign_role', 10)
-        self.get_logger().info("WOLF GAME (6 players)")
-        self.get_logger().info("2 villagers, 2 wolves, 1 witch, 1 predictor")
+        # night actions
+        self.night_start_pub_ = self.create_publisher(String, 'night_start', 10)
+        self.wolf_action_sub_ = self.create_subscription(String, 'wolf_action', self.wolf_action_callback, 10)
+
+        self.get_logger().info("WOLF GAME (5 players)")
+        self.get_logger().info("4 villagers, 1 wolf")
              
+    ### Step1: players ready
     def player_ready_callback(self, msg):
         player_name = msg.data
         if player_name not in self.players_ready_:
             self.players_ready_.append(player_name)
             self.get_logger().info(f"{player_name} is ready")
-        # Assign roles when all 6 players are ready
-        if len(self.players_ready_) == 6 and not self.roles_assigned_:
+        # Assign roles when all 5 players are ready
+        if len(self.players_ready_) == 5 and not self.roles_assigned_:
             self.assign_rloe()
             self.roles_assigned_ = True
+            # start of night actions, this will also triggered only once HERE
+            time.sleep(1)
+            self.start_night_phase()
             
 
     def assign_rloe(self):
@@ -35,7 +49,75 @@ class GameMasterNode(Node): # MODIFY NAME
             msg = String()
             msg.data = f"{player}:{role}"
             self.roles_pub_.publish(msg)
-        self.get_logger().info(f"All 6 players ready. Roles are assigned.")
+        
+            # Store player states
+            self.player_roles_[player] = role
+            self.player_states_[player] = 'alive'
+
+            # Initialize night action
+            if role == "villager":
+                self.player_action_[player] = "done"  # villagers have no night action
+            else:
+                self.player_action_[player] = None    # non-villagers need to act
+
+        self.get_logger().info(f"All 5 players ready. Roles are assigned.")
+
+    ### Step2: night actions
+    def start_night_phase(self):
+        self.game_phase_ = "night"
+        # self.night_done_players_.clear()
+        for i in range(3,0,-1):
+            msg = String()
+            msg.data = f"Night start in {i} s"
+            self.night_start_pub_.publish(msg)
+            self.get_logger().info(msg.data)
+            time.sleep(1)
+        
+        msg = String()
+        msg.data = "🌙 Night phase begins!"
+        self.night_start_pub_.publish(msg)
+        self.get_logger().info(msg.data)
+
+    def wolf_action_callback(self, msg):
+        voter, target = msg.data.split(':')[0], msg.data.split(':')[1]
+
+        # Record vote only if voter is a wolf and alive
+        if self.player_roles_.get(voter) == "wolf" and self.player_states_.get(voter) == 'alive':
+            # Store wolf vote
+            self.wolf_action_[voter] = target
+            # Mark wolf action as done
+            self.player_action_[voter] = "done"
+            # announce who is killed
+            self.get_logger().info(f"Player{target} is killed.")
+
+            # Mark target player as dead
+            self.player_states_[f"player{target}"] = 'dead'
+            
+            # Check if all night actions are done
+            self.night_done()
+
+
+    def night_done(self):     
+        if all(action is not None for action in self.player_action_.values()):   
+            self.get_logger().info("🌞 Night actions complete. Starting day phase...")
+            time.sleep(1)
+            self.start_day_phase()
+        
+
+
+    ### Step3: day actions
+    def start_day_phase(self):
+        self.game_phase_ = "day"
+
+        # List alive players
+
+
+
+        
+        self.get_logger().info("this is day action, gaming continue...")
+        time.sleep(3)
+        self.start_night_phase()
+        
 
 
 def main(args=None):
--- src/pkg_wolf_game/pkg_wolf_game/player4.py
+++ src/pkg_wolf_game/pkg_wolf_game/player4.py
@@ -2,7 +2,6 @@ import rclpy
 from rclpy.node import Node
 from example_interfaces.msg import String
 
-
 class Player4Node(Node): # MODIFY NAME
 
     def __init__(self):
@@ -12,6 +11,10 @@ class Player4Node(Node): # MODIFY NAME
         self.role_ = None
         self.ready_pub_ = self.create_publisher(String, "player_ready", 10) # topic name
         self.role_sub_ = self.create_subscription(String, 'assign_role', self.assign_role_callback, 10)
+        
+        self.night_start_sub_ = self.create_subscription(String, 'night_start', self.night_start_callback, 10)
+        self.wolf_action_pub_ = self.create_publisher(String, 'wolf_action', 10)
+
         self.get_logger().info(f"ready")
         self.create_timer(0.5, self.announce_ready)
 
@@ -25,6 +28,38 @@ class Player4Node(Node): # MODIFY NAME
         if player == self.name:
             self.role_ = role
             self.get_logger().info(f"Your role is: {self.role_}")
+
+    def night_start_callback(self, msg):
+        """Respond to game master's announcements"""
+        data = msg.data
+        self.get_logger().info(data)
+
+        ### night actions
+        # role == villager
+        if data == "🌙 Night phase begins!" and self.role_ == "villager" and self.state_ == "alive":
+            self.get_logger().info("Wait for day time...")
+
+        # role == wolf
+        if data == "🌙 Night phase begins!" and self.role_ == "wolf" and self.state_ == "alive":
+            self.wolf_action()
+        
+
+
+    def wolf_action(self):
+        # Ask user input
+        target = None
+        while target not in range(1, 6):
+            try:
+                target = int(input("Enter player number to kill (1-5): ").strip())
+                if target not in range(1, 6):
+                    print("Invalid choice, try again.")
+            except ValueError:
+                print("Please enter a valid number between 1 and 5.")
+
+        msg = String()
+        msg.data = f"{self.name}:{target}, action:done"
+        self.wolf_action_pub_.publish(msg)
+
              
 
 def main(args=None):
